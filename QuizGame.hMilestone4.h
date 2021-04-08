c8e024bb671ba507959265092ddcfa7233db0536
justinantonio <78823009+justinantonio@users.noreply.github.com>
2021-04-07 23:11:46 -0700

    Add files via upload

---
+++ QuizGame.hMilestone4.h
@@ -0,0 +1,33 @@
+#define PROMPT_INTRO "WELCOME, I HOPE YOU KNOW YOUR ENGINES... I WISH YOU THE BEST OF LUCK!" //intro display
+
+#define PROMPT_QUESTION1A "nissan GTR R34 has an RB26 motor: " //Question 1 
+#define PROMPT_QUESTION1B "nissan GTR R34 does not have an RB26 motor: " //question 1
+
+#define PROMPT_QUESTION2A "toyota MK4 supra has a 2JZ-GTE motor:" //question 2
+#define PROMPT_QUESTION2B "toyota MK4 supra does not have a 2JZ-GTE motor: " //question 2
+
+#define PROMPT_QUESTION3A "nissan R35 GTR has a VR38DETT motor: " //question 3
+#define PROMPT_QUESTION3B "nissan R35 GTR does not have a VR38DETT motor " //question 3
+
+#define PROMPT_QUESTION4A "mazda RX7 has a rotary engine: " //question 4
+#define PROMPT_QUESTION4B "mazda RX7 does not have a rotary engine " //question 4
+
+#define PROMPT_QUESTION5A "nissan S15 silvia has a SR20DET motor: " //question 5
+#define PROMPT_QUESTION5B "nissan S15 silvia does not have a SR20DET motor " //question 5
+
+#define PROMPT_QUESTION6A "nissan 350z does have a v8 motor stock: " //question 6
+#define PROMPT_QUESTION6B "nissan 350z does not have a v8 motor stock " //question 6
+
+#define PROMPT_QUESTION7A "mitsubishi evo VIII has a vq35 motor: " //question 7
+#define PROMPT_QUESTION7B "mitsubishi evo VIII does not have a VQ motor" //question 7
+
+#define PROMPT_QUESTION8A "subaru STI has a 1JZ-GTE motor: " //question 8
+#define PROMPT_QUESTION8B "subaru STI does not have a 1JZ-GTE motor" //question 8
+
+#define PROMPT_QUESTION9A "Mercedes C63 AMG has a SR20DET motor: " //question 9
+#define PROMPT_QUESTION9B "Mercedes C63 AMG does not have a SR20DET motor" //question 9
+
+#define PROMPT_QUESTION10A "lamborghini huracan has a F136 motor: " //question 10
+#define PROMPT_QUESTION10B "lamborghini huracan does not have a F136 motor" //question 10
+
+#define PROMPT_OUTRO "CONGRATULATIONS YOU HAVE FINISHED THIS QUIZ\n" //end display
